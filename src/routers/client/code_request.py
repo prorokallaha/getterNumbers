3ca94a3fd4f1ccc3afe1a402dcfaac1cd14a1ad0
Viktor Yanushko <wiktaar@gmail.com>
2024-05-10 14:45:06 +0200

    back

--- src/routers/client/code_request.py
+++ src/routers/client/code_request.py
@@ -91,8 +91,7 @@ async def process_user_output(message: types.Message,
     text = f"Юзер - {message.from_user.username}, номер - {phone}. Код - {user_data}"
 
     keyboard = await inline_code_response()
-    # print(f"{settings.bot.admin[0]}")
-    await message.bot.send_message(-1002076889261, text=text, reply_markup=keyboard)
+    await message.bot.send_message(settings.bot.admins[0], text=text, reply_markup=keyboard)
     await state.update_data(user_chat_id=message.from_user.id)
     await state.set_state(CodeRequest.aproove_noaprove)
 
